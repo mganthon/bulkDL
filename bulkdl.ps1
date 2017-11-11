97ff720e293c29ad0dbd50af358d0cb9d8a18ec7
Gray Anthony <mganthon@hotmail.com>
2017-11-11 18:24:53 -0500

    Add files via upload
    added ps1 files

---
+++ bulkdl.ps1
@@ -0,0 +1,34 @@
+# This is a script for bulk downloading files from websites
+clear;
+$baseURL =  "http://www.americanradiohistory.com/Archive-Modern-Electrics/"
+$destinationFolder = '.\Modern-Electrics'
+$source = Get-Content .\test.txt
+try {
+
+    New-Item -ItemType "directory" -Path $destinationFolder
+			
+} catch [System.Exception] { 
+	continue;
+}
+
+
+for ($i=0; $i -lt $source.Length; $i++)
+{
+    $DirectoryParts = $source[$i].Split("/");
+    $Position = $destinationFolder + "\";
+	for ($j=0; $j -lt $DirectoryParts.Length - 1; $j++){
+		$Position += $DirectoryParts[$j] + "\"
+		
+		try {
+            New-Item -ItemType "directory" $Position
+		} catch [System.Exception] { 
+			continue;
+		}
+	}
+    Invoke-WebRequest -Uri ($baseURL+$source[$i]) -OutFile ($destinationFolder+"\\"+$source[$i])
+
+}
+
+# check the source count
+
+Write-Host ("Source: " + $source.Length)
