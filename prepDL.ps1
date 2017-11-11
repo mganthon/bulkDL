22ef371f16c6bf5ec055568d345aed412ac31733
Gray Anthony <mganthon@hotmail.com>
2017-11-11 18:26:27 -0500

    Update prepDL.ps1
    cleaned up prep file and provided comments

--- prepDL.ps1
+++ prepDL.ps1
@@ -1,4 +1,4 @@
 ﻿clear;
-$rawURL =  "http://www.americanradiohistory.com/Modern_Electrics_Magazine.htm"
-$output = './test.txt'
-Invoke-WebRequest -Uri $rawURL | Select-String -Pattern 'href="([\S]+[\s\S]pdf)' -AllMatches |% { $_.Matches } | % { $_.Value } |  Out-File $output
+$rawURL =  "http://yourURLhere/pageOfInterest.htm"
+$outputFile = './test.txt'
+Invoke-WebRequest -Uri $rawURL | Select-String -Pattern 'href="([\S]+[\s\S]pdf)' -AllMatches |% { $_.Matches } | % { $_.Value } |  Out-File $outputFile
